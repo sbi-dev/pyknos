ed4e89658d3f3f085344f7150e048c8ecc7c39f5
Alvaro Tejero-Cantero <alvaro.tejero@tum.de>
2020-02-21 15:23:15 +0100

    Initial commit: nflows passthrough.

---
+++ pyknos/nflows/__init__.py
@@ -0,0 +1,1 @@
+from nflows import transforms, flows, distributions
---
+++ setup.py
@@ -0,0 +1,35 @@
+# coding: utf-8
+
+from setuptools import find_packages, setup
+
+exec(open("pyknos/version.py").read())
+
+setup(
+    name="pyknos",
+    version=__version__,
+    description="don't regress",
+    url="https://github.com/mackelab/pyknos",
+    author="Álvaro Tejero Cantero",
+    packages=find_packages(exclude=["tests"]),
+    license="GPLv3",
+    install_requires=[
+        "matplotlib",
+        "nflows@git+ssh://git@github.com/mackelab/nflows.git",
+        "numpy",
+        "tensorboard",
+        "torch",
+        "tqdm",
+    ],
+    extras_requires={
+        "dev": [
+            "autoflake",
+            "black",
+            "flake8",
+            "isort",
+            "pytest",
+            "pyyaml",
+            "torchtestcase",
+        ],
+    },
+    dependency_links=[],
+)
