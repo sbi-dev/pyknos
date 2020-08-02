db10a235f829976b4d3161e54b8f2d56be1e7b0a
Alvaro Tejero-Cantero <alvaro.tejero@tum.de>
2020-08-02 18:10:13 +0200

    Prepare for  0.12 release depending on nflows 0.12.

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# v0.12.0
+- Upgraded dependency on `nflows` to 0.12; explicitly allow Python 3.6 (#13)
+- Support for `batch_x` with only one feature in mixture density networks (thanks to
+  Harry Fu, #12)
+- Imports from `nflows.transforms.splines` now available (thanks to Miles Cranmer, #11)
--- pyknos/version.py
+++ pyknos/version.py
@@ -1,2 +1,7 @@
-__version__ = "0.11"
-VERSION = __version__
+# This file is part of pyknos, a library for conditional density estimation.
+# pyknos is licensed under the Affero General Public License v3,
+# see <https://www.gnu.org/licenses/>.
+
+VERSION = (0, 12, 0)
+
+__version__ = ".".join(map(str, VERSION))
