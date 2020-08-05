f8817095e6b04947394ae912a709e695e37c93cf
Harry Fu Yu <43154947+harry-fuyu@users.noreply.github.com>
2020-08-05 18:25:41 +0800

    updated variable names

--- test/mdn_test.py
+++ test/mdn_test.py
@@ -1,11 +1,8 @@
-from __future__ import annotations
-
 import pytest
 import torch
-from torch import Tensor, eye, ones, zeros
 import torch.nn as nn
+from torch import Tensor, eye, ones, zeros
 from torch.distributions import MultivariateNormal
-
 from pyknos.mdn.mdn import MultivariateGaussianMDN
 
 
@@ -25,20 +22,19 @@ def test_mdn_for_diff_dimension_data(dim: int,
             hidden_features: int=50, 
             num_components: int=10) -> None:
 
-    theta = torch.rand(3, dim)
-    likelihood_shift = torch.zeros(theta.shape)
+    y = torch.rand(3, dim)
+    likelihood_shift = torch.zeros(y.shape)
     likelihood_cov = eye(dim)
-    context =  linear_gaussian(theta, likelihood_shift, likelihood_cov)
+    x =  linear_gaussian(y, likelihood_shift, likelihood_cov)
 
-    x_numel = theta[0].numel()
-    y_numel = context[0].numel()
+    x_numel = x[0].numel()
     
     distribution = MultivariateGaussianMDN(
-        features=x_numel,
-        context_features=y_numel,
+        features=dim,
+        context_features=x_numel,
         hidden_features=hidden_features,
         hidden_net=nn.Sequential(
-            nn.Linear(y_numel, hidden_features),
+            nn.Linear(x_numel, hidden_features),
             nn.ReLU(),
             nn.Linear(hidden_features, hidden_features),
             nn.ReLU(),
@@ -47,9 +43,9 @@ def test_mdn_for_diff_dimension_data(dim: int,
         custom_initialization=True,
     )
 
-    logits, means, precisions, _, _ = distribution.get_mixture_components(theta)
+    logits, means, precisions, _, _ = distribution.get_mixture_components(y)
     
-    log_prob = distribution.log_prob(context, theta)
+    log_prob = distribution.log_prob(x, y)
 
-    sample = distribution.sample(100, theta)
+    sample = distribution.sample(100, y)
 
