df7fcbee27eb6077eac8d6fad0de933d5ccb8b73
Jan Boelts <jan.boelts@mailbox.org>
2024-08-20 11:18:25 +0200

    bump version, update readme

--- pyknos/__version__.py
+++ pyknos/__version__.py
@@ -1,7 +1,6 @@
 # This file is part of pyknos, a library for conditional density estimation.
-# pyknos is licensed under the Affero General Public License v3,
-# see <https://www.gnu.org/licenses/>.
+# pyknos is licensed under the Apache License 2.0., see LICENSE for more details.
 
-VERSION = (0, 15, 2)
+VERSION = (0, 16, 0)
 
 __version__ = ".".join(map(str, VERSION))
